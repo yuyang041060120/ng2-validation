c87fca94e8e74cbaa6431af135d6f3143a4282a7
yuyang041060120 <yuyang041060120@gmail.com>
2016-07-19 17:23:40 +0800

    add unit test

---
+++ karma-test-shim.js
@@ -0,0 +1,21 @@
+Error.stackTraceLimit = Infinity;
+
+require('core-js/es6');
+require('reflect-metadata');
+require('rxjs');
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/async-test');
+
+var appContext = require.context('./src/specs', true, /\.spec\.ts/);
+
+appContext.keys().forEach(appContext);
+
+var testing = require('@angular/core/testing');
+var browser = require('@angular/platform-browser-dynamic/testing');
+
+testing.setBaseTestProviders(
+    browser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+    browser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
+);
---
+++ src/specs/custom-validators.spec.ts
@@ -0,0 +1,200 @@
+import { FormControl, ValidatorFn } from '@angular/forms';
+
+import { CustomValidators } from '../custom-validators';
+
+describe('Custom Validators RangeLength [4,9],', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.rangeLength([4, 9]);
+    });
+
+    it('"abc" should equal to "{rangelength: true}"', () => {
+        control = new FormControl('abc');
+        expect(validator(control)).toEqual({rangelength: true});
+    });
+
+    it('"abcd" should equal to "null"', () => {
+        control = new FormControl('abcd');
+        expect(validator(control)).toBeNull();
+    });
+
+    it('"abcdefghi" should equal to "null"', () => {
+        control = new FormControl('abcdefghi');
+        expect(validator(control)).toBeNull();
+    });
+
+    it('"abcdefghij" should equal to "{rangelength: true}"', () => {
+        control = new FormControl('abcdefghij');
+        expect(validator(control)).toEqual({rangelength: true});
+    });
+});
+
+describe('Custom Validators Min 10,', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.min(10);
+    });
+
+    it('"9" should equal to "{min: true}"', () => {
+        control = new FormControl(9);
+        expect(validator(control)).toEqual({min: true});
+    });
+
+    it('"10" should equal to "null"', () => {
+        control = new FormControl(10);
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"11" should equal to "null"', () => {
+        control = new FormControl(11);
+        expect(validator(control)).toBeNull()
+    });
+});
+
+describe('Custom Validators Max 20,', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.max(20);
+    });
+
+    it('"19" should equal to "null"', () => {
+        control = new FormControl(19);
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"20" should equal to "null"', () => {
+        control = new FormControl(20);
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"21" should equal to "{max: true}"', () => {
+        control = new FormControl(21);
+        expect(validator(control)).toEqual({max: true});
+    });
+});
+
+describe('Custom Validators Range [4,9],', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.range([4, 9]);
+    });
+
+    it('"3" should equal to "{range: true}"', () => {
+        control = new FormControl(3);
+        expect(validator(control)).toEqual({range: true});
+    });
+
+    it('"4" should equal to "null"', () => {
+        control = new FormControl(4);
+        expect(validator(control)).toBeNull();
+    });
+
+    it('"9" should equal to "null"', () => {
+        control = new FormControl(9);
+        expect(validator(control)).toBeNull();
+    });
+
+    it('"10" should equal to "{range: true}"', () => {
+        control = new FormControl(10);
+        expect(validator(control)).toEqual({range: true});
+    });
+});
+
+describe('Custom Validators Digits,', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.digits;
+    });
+
+    it('"23" should equal to "null"', () => {
+        control = new FormControl(23);
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"23a" should equal to "{digits: true}"', () => {
+        control = new FormControl('23a');
+        expect(validator(control)).toEqual({digits: true});
+    });
+});
+
+describe('Custom Validators Number,', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.number;
+    });
+
+    it('"23" should equal to "null"', () => {
+        control = new FormControl('23');
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"23.3" should equal to "null"', () => {
+        control = new FormControl('23.3');
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"23a" should equal to "{number: true}"', () => {
+        control = new FormControl('23a');
+        expect(validator(control)).toEqual({number: true});
+    });
+
+    it('"23." should equal to "{number: true}"', () => {
+        control = new FormControl('23.');
+        expect(validator(control)).toEqual({number: true});
+    });
+});
+
+describe('Custom Validators Url,', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.url;
+    });
+
+    it('"http://www.test.com" should equal to "null"', () => {
+        control = new FormControl('http://www.test.com');
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"https://www.test.com" should equal to "null"', () => {
+        control = new FormControl('https://www.test.com');
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"23a" should equal to "{url: true}"', () => {
+        control = new FormControl('23a');
+        expect(validator(control)).toEqual({url: true});
+    });
+});
+
+describe('Custom Validators Email,', () => {
+    let control: FormControl;
+    let validator: ValidatorFn;
+
+    beforeEach(() => {
+        validator = CustomValidators.email;
+    });
+
+    it('"tester@gmail.com" should equal to "null"', () => {
+        control = new FormControl('tester@gmail.com');
+        expect(validator(control)).toBeNull()
+    });
+
+    it('"testergmail" should equal to "{email: true}"', () => {
+        control = new FormControl('testergmail');
+        expect(validator(control)).toEqual({email: true});
+    });
+});
