39c89c2b1d7afd700d25bd041a4c7f8f1d803fa0
Sefi Ninio <sefi.ninio@gmail.com>
2016-12-14 17:28:17 +0200

    Fix email validator regex

--- src/custom-validators.ts
+++ src/custom-validators.ts
@@ -88,7 +88,7 @@ export class CustomValidators {
     if (isPresent(Validators.required(control))) return null;
 
     let v: string = control.value;
-    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(v) ? null : {'email': true};
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) ? null : {'email': true};
   }
 
   /**
