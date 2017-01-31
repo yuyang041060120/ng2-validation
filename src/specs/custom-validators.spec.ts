1b0e19a4af00ae57ec52d3b46e6bd30b3ff67418
Dominic Watson <dominic.watson@xcaliber.com>
2017-01-31 17:46:15 +0100

    Add notEqualTo validator
    Closes #45

--- src/specs/custom-validators.spec.ts
+++ src/specs/custom-validators.spec.ts
@@ -348,6 +348,45 @@ describe('Custom Validators EqualTo,', () => {
 
 });
 
+describe('Custom Validators NotEqualTo,', () => {
+  let notEqualControl: FormControl;
+  let control: FormControl;
+  const error = {notEqualTo: true};
+
+  beforeEach(() => {
+    notEqualControl = new FormControl();
+    control = new FormControl();
+  });
+
+  it('all control is empty should valid', () => {
+    expect(CustomValidators.notEqualTo(notEqualControl)(control)).toEqual(error);
+  });
+
+  it('control.value = "xxx" and notEqualControl.value is empty should valid', () => {
+    control.setValue('xxx');
+    expect(CustomValidators.notEqualTo(notEqualControl)(control)).toBeNull();
+  });
+
+  it('control.value = "xxx" and notEqualControl.value = "yyy" should valid', () => {
+    control.setValue('xxx');
+    notEqualControl.setValue('yyy');
+    expect(CustomValidators.notEqualTo(notEqualControl)(control)).toBeNull();
+  });
+
+  it('control.value = "xxx" and notEqualControl.value = "xxx" should equal to "{notEqualTo: true}"', () => {
+    control.setValue('xxx');
+    notEqualControl.setValue('xxx');
+    expect(CustomValidators.notEqualTo(notEqualControl)(control)).toEqual(error);
+  });
+
+  it('control.value is empty and notEqualControl.value = "yyy" should valid', () => {
+    control.setValue('');
+    notEqualControl.setValue('yyy');
+    expect(CustomValidators.notEqualTo(notEqualControl)(control)).toBeNull();
+  });
+
+});
+
 describe('Custom Validators phone,', () => {
   let control: FormControl;
   let validator: ValidatorFn;
