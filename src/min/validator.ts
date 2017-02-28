e5df9d41b4495c6cbbd718e159aa2622132ed422
Shahmir Noorani <SNoorani@slb.com>
2017-02-28 10:54:10 -0600

    Fix: According to MDN at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input, min and max can take a date-time value.
    When that happens, these two validators ends up trying to convert these values into a number, which results in NaN, which causes the validation to fail.
    
    To fix this issue, if the min or max value isNaN and is a date, then simply return null. The validtion of these fields are handled by minDate and maxDate

--- src/min/validator.ts
+++ src/min/validator.ts
@@ -1,11 +1,12 @@
 import { AbstractControl, Validators, ValidatorFn } from '@angular/forms';
 
-import { isPresent } from '../facade/lang';
+import { isPresent, isDate } from '../facade/lang';
 
 export const min = (min: number): ValidatorFn => {
   return (control: AbstractControl): {[key: string]: boolean} => {
     if (!isPresent(min)) return null;
     if (isPresent(Validators.required(control))) return null;
+    if (isNaN(min) && isDate(new Date(min))) return null;
 
     let v: number = +control.value;
     return v >= +min ? null : {min: true};
