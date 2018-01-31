d40cf04495d464a660d31ccedf326545bd80e541
gurolg <gurol@gurkan.org>
2018-01-31 14:41:46 +0300

    Update validator.ts
    TROY Card validation added
    ref: https://en.wikipedia.org/wiki/Payment_card_number
    TROY	979200–979289	16 Digits

--- src/app/credit-card/validator.ts
+++ src/app/credit-card/validator.ts
@@ -13,7 +13,7 @@ export const creditCard: ValidatorFn = (control: AbstractControl): {[key: string
 
   // problem with chrome
   /* tslint:disable */
-  if (!(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(sanitized))) {
+  if (!(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11}|(?:9792)\d{12})$/.test(sanitized))) {
     return {creditCard: true};
   }
   /* tslint:enable */
